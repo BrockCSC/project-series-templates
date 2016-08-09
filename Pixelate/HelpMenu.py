9ce1db7c024e0231d43062895b816d4bacded73d
Adam Balint <Adam@Adams-MacBook-Air.local>
2016-08-08 22:55:13 -0400

    Started working on UI

---
+++ Pixelate/HelpMenu.py
@@ -0,0 +1,13 @@
+def print_controls():
+	print ("Function \t\t Command\t\tExample")
+	print ("----------------------------------------------------")
+	print ("Quit Program \t\t quit or exit")
+	print ("Setting Image \t\t image name.extension \t\t image puppy.jpg")
+	print ("Set Input Directory \t\t in_dir directory_path \t\t in_dir Images")
+	print("\n\n")
+
+def pixelate_help():
+	print ("Input \t\t\t pixelate pixel_size color_selection_method")
+	print ("pixel_size \t\t integer value (ex. 5) \n \t\t\t makes pixel to be that size square (ex. 5x5)")
+	print ("color_selection_method \t max, min, mean_col")
+	print("\n")
